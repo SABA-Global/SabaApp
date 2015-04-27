8642379597202e79a08f408b2942e35adc890f13
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2015-04-27 00:43:44 -0700

    Removed dead code.

--- SabaApp/WeeklyScheduleViewController.m
+++ SabaApp/WeeklyScheduleViewController.m
@@ -29,19 +29,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 -(void) onBack{
 	NSLog(@"Back button clicked...");
 	[self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
